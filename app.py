651a2a48ec694ddeee9045299e7b360a79cd0a22
Pablo Acevedo <pacevedo@gmail.com>
2021-11-25 16:33:16 -0600

    fix syntax

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 
-From flask import Flask
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world:
+def hello_world():
     return 'Hey, we have Flask in a Docker container!'
 
 
